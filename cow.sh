a1cd0612185a7324f5625ece151ec56838462827
abu <igohabubakar@gmail.com>
2024-08-07 10:45:43 +0100

    Update cow.sh

--- cow.sh
+++ cow.sh
@@ -4,4 +4,5 @@ read -p "what state are you from" state
 read -p "what is youe age" year_of_birth
 age=$((current_age-year_of_birth))
 echo "your $name and your $state leading to you $age"
-
+this is what i want to see
+let's get started
